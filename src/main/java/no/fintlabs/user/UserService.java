5680a81c2069750deae8c135d0fce11b55677075
Erling Jahr <erling.jahr@gmail.com>
2022-11-23 12:08:47 +0100

    added consumer for User

---
+++ src/main/java/no/fintlabs/user/UserService.java
@@ -0,0 +1,15 @@
+package no.fintlabs.user;
+
+import lombok.extern.slf4j.Slf4j;
+import no.fintlabs.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserService {
+    int x = 0;
+    public void process(User user) {
+      //log.info(user.getUserId());
+      log.info(String.valueOf(x++));
+    }
+}
