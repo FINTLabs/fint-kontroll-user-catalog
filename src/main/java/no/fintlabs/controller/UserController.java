bf2cf94a8ac4a62642bfdad7042ea4a5f8470c20
Erling Jahr <erling.jahr@gmail.com>
2023-01-13 15:31:45 +0100

    refactor urls for returning list of users

--- src/main/java/no/fintlabs/controller/UserController.java
+++ src/main/java/no/fintlabs/controller/UserController.java
@@ -7,6 +7,7 @@ import no.fintlabs.dto.UserDTOforList;
 import no.fintlabs.user.User;
 import no.fintlabs.user.UserService;
 import no.vigoiks.resourceserver.security.FintJwtEndUserPrincipal;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
@@ -30,11 +31,14 @@ public class UserController {
         this.fintFilterService = fintFilterService;
     }
 
-    @GetMapping
-    public Flux<UserDTOforList> getAllUserDTOforList(@AuthenticationPrincipal Jwt jwt){
-        log.info("Fetching all user DTOs");
-        return userService.getAllUsersDTOforList(FintJwtEndUserPrincipal.from(jwt));
-    }
+    @Value("${fint.kontroll.user-catalog.pagesize}")
+    int pagesize;
+
+//    @GetMapping
+//    public Flux<UserDTOforList> getAllUserDTOforList(@AuthenticationPrincipal Jwt jwt){
+//        log.info("Fetching all user DTOs");
+//        return userService.getAllUsersDTOforList(FintJwtEndUserPrincipal.from(jwt));
+//    }
     @GetMapping({"/id/{id}"})
     public Mono<UserDTOforDetails> getUserDTOforDetailsById(@AuthenticationPrincipal Jwt jwt, @PathVariable Long id){
         log.info("Fetchind DTO for user by id");
@@ -53,20 +57,32 @@ public class UserController {
         return Flux.fromStream(filteredResult);
     }
 
-    @GetMapping("/employee")
+    @GetMapping({"/students"})
+    public ResponseEntity<Map<String,Object>> getAllStudentsPaged(@AuthenticationPrincipal Jwt jwt,
+                                                                  @RequestParam(defaultValue = "0") int page,
+                                                                  @RequestParam(defaultValue = "1") int size){
+        if (size==1){size=pagesize;};
+        log.info("Finding " +size+ " of all students at page: " + page);
+        String userType = "STUDENT";
+        return userService.getAllUserByTypePaged(FintJwtEndUserPrincipal.from(jwt),page,size,userType);
+    }
+
+    @GetMapping({"/employees"})
     public ResponseEntity<Map<String,Object>> getAllEmployeesPaged(@AuthenticationPrincipal Jwt jwt,
                                                                    @RequestParam(defaultValue = "0") int page,
-                                                                   @RequestParam(defaultValue = "3") int size){
-        log.info("Find all employees at page: " + page);
+                                                                   @RequestParam (defaultValue = "1") int size){
+        if (size == 1){ size = pagesize;};
+        log.info("Finding " +size+ " of all employees at page: " + page);
         String userType = "EMPLOYEE";
         return userService.getAllUserByTypePaged(FintJwtEndUserPrincipal.from(jwt),page,size, userType);
     }
 
-    @GetMapping("/pageing")
+    @GetMapping
     public ResponseEntity<Map<String, Object>> getUsersPagable(@AuthenticationPrincipal Jwt jwt,
                                                                @RequestParam(defaultValue = "0") int page,
-                                                               @RequestParam(defaultValue = "3") int size) {
-        log.info("Finding all users at page: " + page);
+                                                               @RequestParam(defaultValue = "1") int size) {
+        if (size == 1){ size = pagesize;};
+        log.info("Finding " +size+ " of all users at page: " + page);
         return userService.getAllUsersPaged(FintJwtEndUserPrincipal.from(jwt), page, size);
     }
 
