3938e5f71da88f2962fd1d6a942e749eb6295674
Frode Sjovatsen <fsjovatsen@users.noreply.github.com>
2023-05-09 21:35:38 +0200

    We now ensure member topic

--- src/main/java/no/fintlabs/member/MemberEntityProducerService.java
+++ src/main/java/no/fintlabs/member/MemberEntityProducerService.java
@@ -5,20 +5,32 @@ import no.fintlabs.kafka.entity.EntityProducer;
 import no.fintlabs.kafka.entity.EntityProducerFactory;
 import no.fintlabs.kafka.entity.EntityProducerRecord;
 import no.fintlabs.kafka.entity.topic.EntityTopicNameParameters;
+import no.fintlabs.kafka.entity.topic.EntityTopicService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Service
 @Slf4j
 public class MemberEntityProducerService {
     private final EntityProducer<Member> entityProducer;
     private final EntityTopicNameParameters entityTopicNameParameters;
-    public MemberEntityProducerService( EntityProducerFactory entityProducerFactory)
-    {
+
+    private final EntityTopicService entityTopicService;
+
+
+    public MemberEntityProducerService(EntityProducerFactory entityProducerFactory, EntityTopicService entityTopicService) {
         entityProducer = entityProducerFactory.createProducer(Member.class);
+        this.entityTopicService = entityTopicService;
         entityTopicNameParameters = EntityTopicNameParameters
                 .builder()
                 .resource("member")
                 .build();
+    }
+
+    @PostConstruct
+    public void init() {
+        entityTopicService.ensureTopic(entityTopicNameParameters, 0);
     }
 
     public void publish(Member member) {
