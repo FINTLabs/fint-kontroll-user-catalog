db812305d2aa3669b4f34f610056daa045a98ff4
Morten Solberg <morten@source-code.no>
2023-09-04 11:33:00 +0200

    Adjust test to get users from the API

--- src/test/java/no/fintlabs/user/SecurityTestConfig.java
+++ src/test/java/no/fintlabs/user/SecurityTestConfig.java
@@ -2,8 +2,22 @@ package no.fintlabs.user;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 @TestConfiguration
 public class SecurityTestConfig {
@@ -11,8 +25,65 @@ public class SecurityTestConfig {
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
+                .securityContextRepository(securityContextRepository())
                 .authorizeExchange()
                 .anyExchange().permitAll();
         return http.build();
+    }
+
+    @Bean
+    public ServerSecurityContextRepository securityContextRepository() {
+        return new MockServerSecurityContextRepository();
+    }
+
+    private static class MockServerSecurityContextRepository implements ServerSecurityContextRepository {
+
+        @Override
+        public Mono<Void> save(ServerWebExchange exchange, SecurityContext context) {
+            throw new UnsupportedOperationException("Not supported");
+        }
+
+        @Override
+        public Mono<SecurityContext> load(ServerWebExchange exchange) {
+            String tokenValue =
+                    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ" +
+                    ".SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("sub", "1234567890");
+            claims.put("name", "John Doe");
+
+            Map<String, Object> headers = new HashMap<>();
+            headers.put("alg", "none");
+            headers.put("typ", "JWT");
+
+            Jwt jwt = new Jwt(tokenValue, Instant.now(), Instant.now().plusSeconds(86400), headers, claims);
+
+            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+            securityContext.setAuthentication(new JwtAuthenticationToken(jwt, Collections.emptyList()));
+
+            return Mono.just(securityContext);
+        }
+    }
+
+    private static class JwtAuthenticationToken extends AbstractAuthenticationToken {
+
+        private final Jwt jwt;
+
+        public JwtAuthenticationToken(Jwt jwt, Collection<? extends GrantedAuthority> authorities) {
+            super(authorities);
+            this.jwt = jwt;
+            setAuthenticated(true);
+        }
+
+        @Override
+        public Object getCredentials() {
+            return jwt.getTokenValue();
+        }
+
+        @Override
+        public Object getPrincipal() {
+            return jwt;
+        }
     }
 }
--- src/test/java/no/fintlabs/user/UserControllerTest.java
+++ src/test/java/no/fintlabs/user/UserControllerTest.java
@@ -3,22 +3,23 @@ package no.fintlabs.user;
 
 import no.vigoiks.resourceserver.security.FintJwtEndUserPrincipal;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import java.util.*;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 @Import(SecurityTestConfig.class)
@@ -27,8 +28,7 @@ public class UserControllerTest {
 
     @Autowired
     private WebTestClient webTestClient;
-    @Autowired
-    private JwtDecoder jwtDecoder;
+
 
     @MockBean
     private ResponseFactory responseFactory;
@@ -37,30 +37,37 @@ public class UserControllerTest {
     private UserService userService;
 
 
-
     @Test
-    void testEndpoint_whenUserScopesFound_shouldReturn200() {
+    void shouldGetUsers() {
+        // TODO: Kanskje greit å ha noe slags brukere i denne listen?
+        Map<String, Object> expextedRespons = new HashMap<>();
+        expextedRespons.put("responsKey", "responsValue");
 
-
-     String jwtString = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
-
-
-
-        Jwt jwtMock = jwtDecoder.decode(jwtString);
-        Map<String,Object> expextedRespons = new HashMap<>();
-        expextedRespons.put("responsKey","responsValue");
-
-        given(responseFactory.toResponseEntity(FintJwtEndUserPrincipal.from(jwtMock),"%",0,20))
+        given(responseFactory.toResponseEntity(any(FintJwtEndUserPrincipal.class),
+                                               anyString(),
+                                               anyList(),
+                                               anyString(),
+                                               anyInt(),
+                                               anyInt()))
                 .willReturn(ResponseEntity.ok(expextedRespons));
 
+        List<String> orgUnits = Arrays.asList("unit1", "unit2", "unit3");
+
         webTestClient.get()
-                .uri("api/users")
+                .uri(uriBuilder -> {
+                    uriBuilder.path("/api/users");
+                    uriBuilder.queryParam("search", "test");
+                    uriBuilder.queryParam("orgUnits", orgUnits.toArray());
+                    uriBuilder.queryParam("userType", "test");
+                    uriBuilder.queryParam("page", 0);
+                    uriBuilder.queryParam("size", 20);
+                    return uriBuilder.build();
+                })
                 .accept(MediaType.APPLICATION_JSON)
-                .header("Authorization", jwtMock.getTokenValue())
+                .header("Authorization", "Bearer dummy jwt token")
                 .exchange()
                 .expectStatus().isOk()
                 .expectBody(Map.class)
                 .isEqualTo(expextedRespons);
     }
-
 }
