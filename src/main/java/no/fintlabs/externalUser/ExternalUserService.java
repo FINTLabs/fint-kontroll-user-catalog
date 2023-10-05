856ddc3b8311b1533cdefd42aa4e811ff1870ec9
Erling Jahr <erling.jahr@vigoiks.no>
2023-10-05 10:54:05 +0200

    added save/update methode to service

--- src/main/java/no/fintlabs/externalUser/ExternalUserService.java
+++ src/main/java/no/fintlabs/externalUser/ExternalUserService.java
@@ -3,10 +3,45 @@ package no.fintlabs.externalUser;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.function.Consumer;
+
+import static com.ibm.icu.text.PluralRules.Operand.e;
+
 @Service
 @Slf4j
 public class ExternalUserService {
+    private final ExternalUserRepository externalUserRepository;
+
+    public ExternalUserService(ExternalUserRepository externalUserRepository) {
+        this.externalUserRepository = externalUserRepository;
+    }
 
     public void save(ExternalUser externalUser) {
+        externalUserRepository
+                .findExternalUserByIdentityProviderUserObjectId(externalUser.getIdentityProviderUserObjectId())
+                .ifPresentOrElse(onSaveExistingExternalUser(externalUser), onSaveNewExternalUser(externalUser));
     }
+
+    private Runnable onSaveNewExternalUser(ExternalUser externalUser){
+        return ()-> {
+          ExternalUser newExternalUser = externalUserRepository.save(externalUser);
+          log.info("Created new external user" + newExternalUser.getUserName());
+        };
+    }
+
+    private Consumer<ExternalUser> onSaveExistingExternalUser(ExternalUser externalUser){
+        return existingExternalUser -> {
+          externalUser.setId(existingExternalUser.getId());
+          log.info("Updating external user " + externalUser.getUserName());
+          externalUserRepository.save(externalUser);
+        };
+    }
+
+    public Optional<ExternalUser> getExternalUserById(Long id){
+        return externalUserRepository.findById(id);
+    }
+
+
+
 }
