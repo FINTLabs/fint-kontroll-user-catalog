4746a3d3605f4594a97c428fc8252d1e8b35588a
Erling Jahr <erling.jahr@vigoiks.no>
2023-11-20 20:28:58 +0100

    fetch user from db before publish

--- src/main/java/no/fintlabs/user/UserService.java
+++ src/main/java/no/fintlabs/user/UserService.java
@@ -42,7 +42,7 @@ public class UserService {
     private Runnable onSaveNewUser(User user) {
         return () -> {
             User newUser = userRepository.save(user);
-            log.info("Create new user: " + user.getId());
+            //log.info("Create new user: " + user.getId());
             memberService.process(memberService.create(newUser));
             userEntityProducerService.publish(newUser);
         };
@@ -51,10 +51,11 @@ public class UserService {
     private Consumer<User> onSaveExistingUser(User user) {
         return existingUser -> {
             user.setId(existingUser.getId());
-            log.info("Update user: " + user.getId());
+            //log.info("Update user: " + user.getId());
             memberService.process(memberService.create(user));
             userRepository.save(user);
-            userEntityProducerService.publish(user);
+            User userToPublish = userRepository.findById(user.getId()).orElse(user);
+            userEntityProducerService.publish(userToPublish);
         };
     }
 
