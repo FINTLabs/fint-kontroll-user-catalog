cafb7222cf55c74d1ae4f87289d3c95ce5ac7b92
chadaj <agata.chadaj@novari.no>
2025-06-05 14:04:09 +0200

    add more tests

--- src/test/java/no/fintlabs/user/UserServiceTest.java
+++ src/test/java/no/fintlabs/user/UserServiceTest.java
@@ -5,12 +5,15 @@ import no.fintlabs.opa.model.Scope;
 import no.vigoiks.resourceserver.security.FintJwtEndUserPrincipal;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.jpa.domain.Specification;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -21,30 +24,34 @@ public class UserServiceTest {
     private UserService userService;
     private UserRepository userRepository;
     private AuthorizationClient authorizationClient;
+    private UserEntityProducerService userEntityProducerService;
+
     @BeforeEach
-    public void init(){
+    public void init() {
         authorizationClient = mock(AuthorizationClient.class);
         userRepository = mock(UserRepository.class);
-        userService = new UserService(userRepository,  null, authorizationClient);
+        userEntityProducerService = mock(UserEntityProducerService.class);
+        userService = new UserService(userRepository, userEntityProducerService, authorizationClient);
     }
+
     @Test
-    public void testGetAllAutorizedOrgUnitIDs() {
+    public void testGetAllAuthorizedOrgUnitIDs() {
         Scope scope1 = Scope.builder()
                 .objectType("user")
-                .orgUnits(List.of("198","2","3"))
+                .orgUnits(List.of("198", "2", "3"))
                 .build();
         Scope scope2 = Scope.builder()
                 .objectType("role")
-                .orgUnits(List.of("198","2","3"))
+                .orgUnits(List.of("198", "2", "3"))
                 .build();
-        List<Scope> scopes= List.of(scope1,scope2);
-        List<String> authorizedOrgIDs = List.of("198","2","3");
+        List<Scope> scopes = List.of(scope1, scope2);
+        List<String> authorizedOrgIDs = List.of("198", "2", "3");
 
         when(authorizationClient.getUserScopesList()).thenReturn(scopes);
 
-        List<String> foundOrgIDs= userService.getAllAutorizedOrgUnitIDs();
+        List<String> foundOrgIDs = userService.getAllAutorizedOrgUnitIDs();
 
-        assertEquals(authorizedOrgIDs,foundOrgIDs);
+        assertEquals(authorizedOrgIDs, foundOrgIDs);
 
         verify(authorizationClient, times(1)).getUserScopesList();
     }
@@ -54,35 +61,35 @@ public class UserServiceTest {
 
         Scope scope1 = Scope.builder()
                 .objectType("user")
-                .orgUnits(List.of("198","2","3"))
+                .orgUnits(List.of("198", "2", "3"))
                 .build();
         Scope scope2 = Scope.builder()
                 .objectType("role")
-                .orgUnits(List.of("198","2","3"))
+                .orgUnits(List.of("198", "2", "3"))
                 .build();
-        List<Scope> scopes= List.of(scope1,scope2);
-        List<String> requestedOrgIDs = List.of("198","2","5");
-        List<String> authorizedOrgIDsForRequest = List.of("198","2");
+        List<Scope> scopes = List.of(scope1, scope2);
+        List<String> requestedOrgIDs = List.of("198", "2", "5");
+        List<String> authorizedOrgIDsForRequest = List.of("198", "2");
         when(authorizationClient.getUserScopesList()).thenReturn(scopes);
 
         List<String> foundOrgIDs = userService.compareRequestedOrgUnitIDsWithOPA(requestedOrgIDs);
 
-        assertEquals(authorizedOrgIDsForRequest,foundOrgIDs);
+        assertEquals(authorizedOrgIDsForRequest, foundOrgIDs);
     }
 
     @Test
-    void testGetDetailedUserById_shouldPermitAccess(){
+    void testGetDetailedUserById_shouldPermitAccess() {
         Scope scope1 = Scope.builder()
                 .objectType("user")
-                .orgUnits(List.of("198","2","3"))
+                .orgUnits(List.of("198", "2", "3"))
                 .build();
         Scope scope2 = Scope.builder()
                 .objectType("role")
-                .orgUnits(List.of("198","2","3"))
+                .orgUnits(List.of("198", "2", "3"))
                 .build();
-        List<Scope> scopes= List.of(scope1,scope2);
-        List<String> requestedOrgIDs = List.of("198","2","5");
-        List<String> authorizedOrgIDsForRequest = List.of("198","2");
+        List<Scope> scopes = List.of(scope1, scope2);
+        List<String> requestedOrgIDs = List.of("198", "2", "5");
+        List<String> authorizedOrgIDsForRequest = List.of("198", "2");
         User requestedUser = User.builder()
                 .id(1L)
                 .userType("STUDENT")
@@ -101,10 +108,22 @@ public class UserServiceTest {
         when(userRepository.findById(1L)).thenReturn(Optional.of(requestedUser));
         DetailedUser requestedDetailedUser = requestedUser.toDetailedUser();
 
-        DetailedUser foundDetailedUser = userService.getDetailedUserById(fintJwtEndUserPrincipal,1L);
+        DetailedUser foundDetailedUser = userService.getDetailedUserById(fintJwtEndUserPrincipal, 1L);
 
         assertEquals(requestedDetailedUser.getId(), foundDetailedUser.getId());
     }
+
+    @Test
+    void testGetDetailedUserById_requestedOrgIDNotInScope() {
+        FintJwtEndUserPrincipal fintJwtEndUserPrincipal = new FintJwtEndUserPrincipal();
+        fintJwtEndUserPrincipal.setMail("titten@tei.no");
+        when(authorizationClient.getUserScopesList()).thenReturn(List.of());
+
+        DetailedUser foundDetailedUser = userService.getDetailedUserById(fintJwtEndUserPrincipal, 1L);
+
+        assertEquals(new DetailedUser(), foundDetailedUser);
+    }
+
     @Test
     void testGetAllUsers_shouldReturnOnlyUsersWithNotNullIdpObjectId() {
         User user1 = User.builder()
@@ -116,9 +135,87 @@ public class UserServiceTest {
                 .id(2L)
                 .build();
 
-        when(userRepository.findAll()).thenReturn(List.of(user1,user2));
+        when(userRepository.findAll()).thenReturn(List.of(user1, user2));
 
         assertEquals(1, userService.getAllUsers().size());
-        assertEquals(user1, userService.getAllUsers().get(0));
+        assertEquals(user1, userService.getAllUsers().getFirst());
     }
+
+    @Test
+    void testCreateNewUserWhenNotFound() {
+
+        User user1 = User.builder()
+                .identityProviderUserObjectId(UUID.fromString("3f2b9b63-47e9-43c2-9d61-dd078d621479"))
+                .resourceId("4711")
+                .build();
+
+        User savedUser = User.builder()
+                .id(1L)
+                .identityProviderUserObjectId(user1.getIdentityProviderUserObjectId())
+                .resourceId(user1.getResourceId())
+                .build();
+
+        when(userRepository.findUserByResourceIdEqualsIgnoreCase(user1.getResourceId()))
+                .thenReturn(Optional.empty());
+        when(userRepository.save(user1)).thenReturn(savedUser);
+
+        userService.save(user1);
+
+        verify(userRepository).save(user1);
+        verify(userEntityProducerService).publish(savedUser);
+    }
+
+    @Test
+    void testUpdateExistingUser() {
+        User existingUser = User.builder()
+                .id(1L)
+                .identityProviderUserObjectId(UUID.fromString("3f2b9b63-47e9-43c2-9d61-dd078d621479"))
+                .resourceId("4711")
+                .build();
+
+        User inputUser = User.builder()
+                .identityProviderUserObjectId(existingUser.getIdentityProviderUserObjectId())
+                .resourceId(existingUser.getResourceId())
+                .build();
+
+        User updatedUser = User.builder()
+                .id(1L)
+                .identityProviderUserObjectId(existingUser.getIdentityProviderUserObjectId())
+                .resourceId(existingUser.getResourceId())
+                .build();
+
+        when(userRepository.findUserByResourceIdEqualsIgnoreCase(existingUser.getResourceId()))
+                .thenReturn(Optional.of(existingUser));
+        when(userRepository.save(inputUser)).thenReturn(updatedUser);
+
+        userService.save(inputUser);
+
+        verify(userRepository).save(inputUser);
+        verify(userEntityProducerService).publish(updatedUser);
+    }
+
+    @Test
+    void testGetSimpleUsersUsingSpec() {
+        String search = "Test";
+        String active = "ACTIVE";
+        String inactive = "INACTIVE";
+        List<String> orgUnits = List.of("ORGUNIT1");
+        List<String> userTypes = List.of("USERTYPE1");
+
+        User activeUser1 = User.builder().id(1L).status(active).build();
+        User inactiveUser = User.builder().id(2L).status(inactive).build();
+        User activeUser2 = User.builder().id(3L).status(active).build();
+
+        List<User> allUsers = List.of(activeUser1, inactiveUser, activeUser2);
+
+        when(userRepository.findAll((Specification<User>) any())).thenReturn(allUsers);
+
+        List<SimpleUser> result = userService.getSimpleUsersUsingSpec(search, orgUnits, userTypes);
+
+        assertEquals(2, result.size());
+        assertTrue(result.contains(activeUser1.toSimpleUser()));
+        assertTrue(result.contains(activeUser2.toSimpleUser()));
+
+    }
+
 }
