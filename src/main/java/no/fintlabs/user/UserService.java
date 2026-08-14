20cd5c7636c0a31d5f2b8a5a5e75cd3b8ce4c386
Erling Jahr <erling.jahr@novari.no>
2026-08-14 12:44:30 +0200

    added logging to update existing users status

--- src/main/java/no/fintlabs/user/UserService.java
+++ src/main/java/no/fintlabs/user/UserService.java
@@ -71,10 +71,14 @@ public class UserService {
 
     private Consumer<User> onSaveExistingUser(FactoryUser incomingUser) {
         return existingUser -> {
+
           User mappedIncoming = mapFromIncomingUser(existingUser, incomingUser);
             log.info("Update existing user: {}", existingUser.getId());
             if(!mappedIncoming.equals(existingUser))
             {
+                if(!existingUser.getStatus().equals(mappedIncoming.getStatus())){
+                    log.info("Status is changed for user: {}, from: {}, to: {}", mappedIncoming.getId(),existingUser.getStatus(),mappedIncoming.getStatus());
+                }
                 User savedUser = userRepository.save(mappedIncoming);
                 userEntityProducerService.publish(savedUser);
             }
@@ -204,12 +208,14 @@ public class UserService {
         var entra = factoryUser.entraStatus();
         var fint  = factoryUser.fintStatus();
 
+
+
         if (UserStatus.DELETED.equals(entra)) {
-            log.info("Status is deleted for user: {}, {}", factoryUser.resourceId(),factoryUser.userName() );
+            log.info("Status is deleted for user: {}, {}, {}", factoryUser.resourceId(),factoryUser.userName(),factoryUser.userType() );
             return UserStatus.DELETED;
         }
         if (UserStatus.INVALID.equals(fint))  {
-            log.info("Status is invalid for user: {}, {}", factoryUser.resourceId(),factoryUser.userName() );
+            log.info("Status is invalid for user: {}, {}, {}", factoryUser.resourceId(),factoryUser.userName(),factoryUser.userType() );
             return UserStatus.INVALID;
         }
 
@@ -219,11 +225,11 @@ public class UserService {
 //                    (factoryUser.validTo()   == null || !factoryUser.validTo().before(now))
 //                    ? UserStatus.ACTIVE
 //                    : UserStatus.DISABLED;
-            log.info("Status is active for user: {}, {}", factoryUser.resourceId(),factoryUser.userName() );
+            log.info("Status is active for user: {}, {}, {}", factoryUser.resourceId(),factoryUser.userName(),factoryUser.userType() );
             return UserStatus.ACTIVE;
         }
 
-        log.info("Status is disabled for user: {}, {}", factoryUser.resourceId(),factoryUser.userName() );
+        log.info("Status is disabled for user: {}, {}, {}", factoryUser.resourceId(),factoryUser.userName(),factoryUser.userType() );
         return UserStatus.DISABLED;
     }
 }
